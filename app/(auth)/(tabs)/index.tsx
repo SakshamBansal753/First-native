5b7ccd7ccbe795623f3067258defd1b6048c5e13
SakshamBansal753 <kanhabansal563@gmail.com>
2026-06-01 03:45:19 +0530

    Completed HomePage

--- app/(auth)/(tabs)/index.tsx
+++ app/(auth)/(tabs)/index.tsx
@@ -1,34 +1,89 @@
 import "@/global.css"
+import dayjs from "dayjs";
 
-import { Text, View } from "react-native";
+import { Image, Text, View,FlatList } from "react-native";
  import { Link } from "expo-router";
-
+import  images  from "@/constants/images";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 import { styled } from "nativewind";
+import { HOME_BALANCE, HOME_SUBSCRIPTIONS, HOME_USER, UPCOMING_SUBSCRIPTIONS } from "@/constants/data";
+import { icons } from "@/constants/icons";
+import { formatCurrency } from "@/lib/utils";
+import ListHeading from "@/components/ListHeading";
+import UpcomingSubscriptions from "@/components/UpcomingSubscriptions";
+import SubscriptionCard from "@/components/SubscriptionCard";
+import { useState } from "react";
+
 const SafeAreaView=styled(RNSafeAreaView)
 export default function App() {
+  const [expandedSubscription,setexpandedSubscription]=useState<string|null>(null);
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewi
-      </Text>
-      <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">
-        Go to OnBoarding
-      </Link>
-       <Link href="/(auth)/Sign-in" className="mt-4 rounded bg-primary text-white p-4">
-        Already have account sign in
-      </Link>
-       <Link href="/(auth)/Sign-up" className="mt-4 rounded bg-primary text-white p-4">
-        Sign UP
-      </Link>
-      <Link href="/subscriptions/spotify">Spotify Subscriptions</Link>
-      <Link 
-      href={{
-        pathname:"/subscriptions/[id]",
-        params:{id:'claude'},
-      }}>
-        Claude Max Subscriptions
-      </Link>
+     
+       
+       
+        <FlatList 
+        ListHeaderComponent={()=>(
+          <>
+           <View className="home-header">
+        <View className="home-user">
+          <Image source={images.avatar} className="home-avatar"/>
+          <Text className="home-user-name">{HOME_USER.name}</Text>
+        </View>
+        <Image source={icons.add} className="home-add-icon"/> 
+      </View>
+      <View className="home-balance-card">
+        <Text className="Home-balance-label">Balance</Text>
+        <View className="home-balance-row">
+          <Text className="home-balance-amount">
+            {formatCurrency(HOME_BALANCE.amount)}
+          </Text>
+          <Text className="home-balance-date">
+            {dayjs(HOME_BALANCE.nextRenewalDate).format('MM/DD')}
+          </Text>
+        </View>
+      </View>
+      <View>
+        <ListHeading title="Upcoming"/>
+        <FlatList 
+        
+        data={UPCOMING_SUBSCRIPTIONS} renderItem={({item})=>(
+          <UpcomingSubscriptions {...item}/>
+
+        )}
+        keyExtractor={(item)=>item.id}
+        horizontal
+        ListEmptyComponent={<Text className="home-empty-state">No Upcoming subscriptions</Text>}/>
+      </View>
+       <ListHeading title="All subscriptions"/>
+          </>
+        )}
+
+        data={HOME_SUBSCRIPTIONS}
+        keyExtractor={(item)=>item.id}
+        renderItem={({ item }) => (
+  <SubscriptionCard
+    {...item}
+    expanded={expandedSubscription === item.id}
+    onPress={() =>
+      setexpandedSubscription((currentId) =>
+        currentId === item.id ? null : item.id
+      )
+    }
+  />
+)}
+extraData={expandedSubscription}
+ItemSeparatorComponent={()=><View className="h-4"/>}
+showsVerticalScrollIndicator={false}
+ListEmptyComponent={
+  <Text className="home-empty-state">
+    No subscription yet
+  </Text>
+}
+  contentContainerStyle={{ paddingBottom: 200 }}/>
+       
+      
+
     </SafeAreaView>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,20 @@
-import { Stack } from "expo-router";
+import { Stack,SplashScreen } from "expo-router";
 import "@/global.css"
+import {useEffect} from "react"
+import { useFocusEffect } from "@react-navigation/native";
+import { useFonts } from "expo-font";
 export default function  RootLayout(){
+const [fontsLoaded] = useFonts({
+  "sans-regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+  "sans-bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+  "sans-medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+  "sans-semiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+});
+  useEffect(()=>{
+    if(fontsLoaded){
+      SplashScreen.hideAsync();
+    }
+  },[fontsLoaded])
+  if(!fontsLoaded){ return null;}
     return <Stack screenOptions={{headerShown:false}} />
 }
